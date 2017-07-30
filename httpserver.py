e693e1912561dcdca51e16ee263208602fc55517
Yao Lu <95luyao@gmail.com>
2017-07-30 20:06:19 +0800

    faster version

--- httpserver.py
+++ httpserver.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
 import itchat
-
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,12 +10,13 @@ api = Api(app)
 class TFRobot(Resource):
     def __init__(self):
         self.login()
+        self.friend_list = itchat.get_friends()
 
     def login(self):
-        itchat.auto_login(enableCmdQR=2)
+        itchat.auto_login(hotReload=True, enableCmdQR=2)
 
-    def _get_username_by_NickName(self, name, friend_list):
-        for friend in friend_list:
+    def _get_username_by_NickName(self, name):
+        for friend in self.friend_list:
             if friend['NickName'] == name:
                 return friend['UserName']
         return None
@@ -25,23 +26,25 @@ class TFRobot(Resource):
         return ret
 
     def send_msg_to_nickname(self, nickname, msg):
-        friend_list = itchat.get_friends()
-        userid = self._get_username_by_NickName(name=nickname, friend_list=friend_list)
+        userid = self._get_username_by_NickName(name=nickname)
         ret = self._send_msg(msg=msg, username=userid)
         return ret
 
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('nickname', type=str)
+        parser.add_argument('name', type=str)
         parser.add_argument('msg', type=str)
         parser.add_argument('token', type=str)
         args = parser.parse_args()
         if args['token'] != 'tensorflow':
             abort(404, message="require token")
         else:
-            return self.send_msg_to_nickname(nickname=args['nickname'], msg=args['msg'])
+            print('Receiving message!')
+            req = self.send_msg_to_nickname(nickname=args['name'], msg=args['msg'])
+            print('Done!')
+            return req 
 
-api.add_resource(TFRobot, '/tensorflow')
+api.add_resource(TFRobot, '/log')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=80)
